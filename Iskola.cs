11905388d821eebde7a50d01d4244421d7ead32b
git <nitslaszlo@gmail.com>
2020-04-23 13:37:49 +0200

    up1

--- Iskola.cs
+++ Iskola.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // 1.f
 
 namespace Iskola
 {
-    class Program
+    class Iskola // 1.f. -> Program.cs forrás átnevezése Iskola.cs, források hozzáadása
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
