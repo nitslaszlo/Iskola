9f81b21df30b2c1d71c9e0604b3ba1478ca506d5
git <nitslaszlo@gmail.com>
2020-04-23 13:46:24 +0200

    up02

--- Iskola.cs
+++ Iskola.cs
@@ -7,10 +7,32 @@ using System.IO; // 1.f
 
 namespace Iskola
 {
+    class Tanuló
+    {
+        public string Kezdés { get; private set; }
+        public string Osztály { get; private set; }
+        public string Név { get; private set; }
+
+        public Tanuló(string sor)
+        {
+            string[] m = sor.Split(';');
+            Kezdés = m[0];
+            Osztály = m[1];
+            Név = m[2];
+        }
+    }
+
     class Iskola // 1.f. -> Program.cs forrás átnevezése Iskola.cs, források hozzáadása
     {
         static void Main(string[] args)
         {
+            // 2. feladat:
+            List<Tanuló> tanulók = new List<Tanuló>();
+            foreach (var i in File.ReadAllLines("../../nevek.txt"))
+            {
+                tanulók.Add(new Tanuló(i));
+            }
+
             Console.ReadKey();
         }
     }
